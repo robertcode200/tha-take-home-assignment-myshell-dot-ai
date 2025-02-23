fb9ffd9545cb11538e03d04caeb03ec24c126db5
robertcode200 <robertcode200@gmail.com>
2025-02-23 23:37:39 +0800

    Add types.ts which includes all the custom types.

---
+++ src/types.ts
@@ -0,0 +1,18 @@
+export type typeDefaultStyleObject = {
+    styleBold: string;
+    styleUnderline: string;
+    styleTextSize: string;
+};
+
+export type typeWordNode = {
+    text: string;
+    styleBold: string;
+    styleUnderline: string;
+    styleTextSize: string;
+};
+
+export type typeResultCheckObject = {
+    isFirstWordBold: boolean;
+    isSecondWordUnderlined: boolean;
+    thirdWordTextSize: string;
+};
