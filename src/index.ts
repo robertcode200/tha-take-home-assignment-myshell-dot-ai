677819437bc8e74a045113089544c5b67090eab0
robertcode200 <robertcode200@gmail.com>
2025-02-24 03:32:12 +0800

    Add the main function, which contains a basic flow/test case to index.ts and invoke it.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,17 @@
 import { runFeatureProcess } from './feature';
 
-console.log('Hello MyShell.AI');
-runFeatureProcess('test-case-1-bold-underlined-text_size_28.docx');
+async function main () {
+    const defaultDocxFilePath = 'test-case-1-bold-underlined-text_size_28.docx';
+
+    const {
+        isFirstWordBold,
+        isSecondWordUnderlined,
+        thirdWordTextSize,
+    } = await runFeatureProcess(defaultDocxFilePath);
+    
+    console.log(`The first word ${isFirstWordBold ? 'is' : 'is not'} bold.`);
+    console.log(`The second word ${isSecondWordUnderlined ? 'is' : 'is not'} underlined.`);
+    console.log(`The third word's text size is ${thirdWordTextSize}.`);
+}
+
+main();
